ad4e3fb922de7f0994d21bfaa41ee7ad33effadf
Yurii Merker <yura.merk11@gmail.com>
2023-05-10 13:17:55 +0300

    Handle errors in main.go and use log.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"sort-files/sort"
 )
 
 func main() {
-	files, _ := sort.GetFilesInDir()
-	num, _ := sort.MoveFilesToDirs(files)
-	fmt.Printf("Script moved %d files to directories.", num)
+	files, err := sort.GetFilesInDir()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	num, err := sort.MoveFilesToDirs(files)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("Script moved %d files to directories.", num)
 }
